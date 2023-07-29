636b8075369877316c9f9887c3e420af089d54b1
watase.atsushi <beechan920@yahoo.co.jp>
2023-07-29 18:09:36 +0900

    FE:reactflow,tailwindcssの導入

---
+++ client/src/components/pages/Flow.tsx
@@ -0,0 +1,74 @@
+import { useCallback } from "react";
+import ReactFlow, {
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  Node,
+  Connection,
+} from "reactflow";
+import "reactflow/dist/style.css";
+
+interface NodeData {
+  label: string;
+}
+
+interface CustomNode extends Node {
+  data: NodeData;
+}
+
+const initialNodes: CustomNode[] = [
+  {
+    id: "start",
+    type: "input",
+    position: { x: 0, y: 0 },
+    data: { label: "start" },
+  },
+  {
+    id: "middle",
+    position: { x: 0, y: 100 },
+    data: { label: "middle" },
+  },
+  {
+    id: "end",
+    type: "output",
+    position: { x: 0, y: 200 },
+    data: { label: "end" },
+  },
+];
+
+const initialEdges = [
+  { id: "e1", source: "start", target: "middle" },
+  { id: "e2", source: "middle", target: "end" },
+];
+
+function Flow() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (connection: Connection) => setEdges((eds) => addEdge(connection, eds)),
+    [setEdges]
+  );
+
+  return (
+    <div style={{ height: "100vh" }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
+        <MiniMap />
+        <Controls />
+        <Background />
+      </ReactFlow>
+    </div>
+  );
+}
+
+export default Flow;
